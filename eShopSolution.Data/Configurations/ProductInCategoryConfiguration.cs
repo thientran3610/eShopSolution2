fa7377815cb8f9c11ed4b093d8bb0a401c06932d
tran minh thien <thientran3610@gmail.com>
2021-03-12 18:42:00 +0700

    Add magration and updata database

---
+++ eShopSolution.Data/Configurations/ProductInCategoryConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Configurations
+{
+    class ProductInCategoryConfiguration : IEntityTypeConfiguration<ProductInCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductInCategory> builder)
+        {
+            builder.ToTable("ProductInCategories");
+
+            builder.HasKey(x => new { x.ProductId, x.CategoryId });
+
+            builder.HasOne(x => x.Product).WithMany(x => x.ProductInCategories)
+                .HasForeignKey(f => f.ProductId);
+
+            builder.HasOne(x => x.Category).WithMany(x => x.ProductInCategories)
+               .HasForeignKey(f => f.CategoryId);
+        }
+    }
+}
---
+++ eShopSolution.Data/Entities/Promotion.cs
@@ -0,0 +1,22 @@
+﻿using eShopSolution.Data.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Promotion
+    {
+        public int Id { get; set; }
+        public string Name { set; get; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public bool ApplyForAll { get; set; }
+        public int? DiscountPercent { set; get; }
+        public decimal? DiscountAmount { set; get; }
+        public Status Status { set; get; }
+
+        public string ProductIds { set; get; }
+        public string ProductCategoryIds { set; get; }
+    }
+}
